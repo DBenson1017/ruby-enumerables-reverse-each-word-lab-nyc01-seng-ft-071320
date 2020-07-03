bec17e50c5bf06c03f49ee0490427c6a3c64af23
Devin Benson <devinbenson@gmail.com>
2020-07-03 17:08:42 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,6 +4,8 @@ def reverse_each_word(string)
   final_array = []
   string_array = string.split("")
   string_array.each do |element|
-    element.reverse 
+    final_array << element.reverse 
+  end 
+  final_array 
   
   
