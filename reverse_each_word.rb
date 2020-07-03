888f4ecb486a05d7ae4ee13837f54d8ae63e786a
Devin Benson <devinbenson@gmail.com>
2020-07-03 17:21:26 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,9 +3,9 @@ require "pry"
 def reverse_each_word(string)
   final_array = []
   string_array = %w[string]
-  binding.pry
   string_array.each do |element|
    element.reverse 
+   binding.pry
    final_array << element
   end 
   final_array.join()
